7419f837156f5da43dd4e49e6a99a1f57ea703e7
Virender Sahu <virender.sahu@hcltech.com>
2025-07-28 01:01:32 +0530

    code refactor

--- src/firebase.js
+++ src/firebase.js
@@ -1,15 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
-// const firebaseConfig = {
-//     apiKey: "AIzaSyCPeY0BJ6hMoR2dpriwMraJjDNKnMQMAFo",
-//     authDomain: "student-crud-app-15e29.firebaseapp.com",
-//     projectId: "student-crud-app-15e29",
-//     storageBucket: "student-crud-app-15e29.firebasestorage.app",
-//     messagingSenderId: "1037577682756",
-//     appId: "1:1037577682756:web:2dcd607dd4a18327b9a111",
-//     measurementId: "G-CKH6Z9JMEE"
-// };
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
